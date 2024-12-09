300bde2f50dfdbe354ee7506d61b5d96752183ae
Pedro-Arthur-GIT <pedroansw@gmail.com>
2024-12-09 13:29:41 -0300

    menu

--- pweb/settings.py
+++ pweb/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ujeti1)63=t#qbh&)9!etb#w@2-4@-20xqr^$w$-&vo%g^_06!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
